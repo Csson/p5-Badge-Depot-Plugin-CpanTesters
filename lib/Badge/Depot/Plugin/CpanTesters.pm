f4be4a5a7bc92f9dff551edc219a860ac9821639
Erik Carlsson <info@code301.com>
2016-02-16 22:53:55 +0000

    Init

---
+++ lib/Badge/Depot/Plugin/CpanTesters.pm
@@ -0,0 +1,28 @@
+use 5.14.0;
+use strict;
+use warnings;
+
+package Badge::Depot::Plugin::CpanTesters;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Badge::Depot::Plugin::CpanTesters;
+
+=head1 DESCRIPTION
+
+Badge::Depot::Plugin::CpanTesters is ...
+
+=head1 SEE ALSO
+
+=cut
